a36c13fb3a9bb0723767f26811aaaeae0c364f8a
hypnotoad <hypnotoad@noemail.net>
2018-09-17 02:21:24 +0000

    Moved the new module autodoc tool to the clay module.
    Added documentatation capture to the build scripts of clay and practcl
    
    Updated the httpd module to get it's doctool parts from clay.
    
    FossilOrigin-Name: 22a614e7a52c374c4e3a24960799775c665eab41181e7fdd0aecec8707b1687c

--- modules/clay/build/doctool.tcl
+++ modules/clay/build/doctool.tcl
@@ -1,13 +1,44 @@
+namespace eval ::clay {}
+
+###
+# Concatenate a file
+###
+proc ::clay::cat fname {
+    if {![file exists $fname]} {
+       return
+    }
+    set fin [open $fname r]
+    set data [read $fin]
+    close $fin
+    return $data
+}
+
+###
+# Append a line of text to a variable. Optionally apply a string mapping.
+# arglist:
+#   map {mandatory 0 positional 1}
+#   text {mandatory 1 positional 1}
+###
+proc ::putb {buffername args} {
+  upvar 1 $buffername buffer
+  switch [llength $args] {
+    1 {
+      append buffer [lindex $args 0] \n
+    }
+    2 {
+      append buffer [string map {*}$args] \n
+    }
+    default {
+      error "usage: putb buffername ?map? string"
+    }
+  }
+}
+
 ###
 # Tool for build scripts to dynamically generate manual files from comments
 # in source code files
 ###
-package provide doctools::build 0.1
-
-namespace eval ::docbuild {}
-
-
-oo::class create ::docbuild::object {
+oo::class create ::clay::doctool {
   constructor {} {
     my variable coro
     set coro [info object namespace [self]]::coro
@@ -15,6 +46,22 @@ oo::class create ::docbuild::object {
     coroutine $coro {*}[namespace code {my reset}]
   }
 
+  ###
+  # Process an argument list into an informational dict.
+  # This method also understands non-positional
+  # arguments expressed in the notation of Tip 471
+  # [uri https://core.tcl-lang.org/tips/doc/trunk/tip/479.md].
+  # [para]
+  # The output will be a dictionary of all of the fields and whether the fields
+  # are [const positional], [const mandatory], and whether they have a
+  # [const default] value.
+  # [para]
+  # [example {
+  #   my arglist {a b {c 10}}
+  #
+  #   > a {positional 1 mandatory 1} b {positional 1 mandatory 1} c {positional 1 mandatory 0 default 10}
+  # }]
+  ###
   method arglist {arglist} {
     set result [dict create]
     foreach arg $arglist {
@@ -57,6 +104,28 @@ oo::class create ::docbuild::object {
     return $result
   }
 
+  ###
+  # Convert a block of comments into an informational dictionary.
+  # If lines in the comment start with a single word ending in a colon,
+  # all subsequent lines are appended to a dictionary field of that name.
+  # If no fields are given, all of the text is appended to the [const description]
+  # field.
+  # Example:
+  # my comment {Does something cool}
+  # > description {Does something cool}
+  #
+  # my comment {
+  # title : Something really cool
+  # author : Sean Woods
+  # author : John Doe
+  # description :
+  # This does something really cool!
+  # }
+  # > description {This does something really cool!}
+  #   title {Something really cool}
+  #   author {Sean Woods
+  #   John Doe}
+  ###
   method comment block {
     set count 0
     set field description
@@ -64,17 +133,26 @@ oo::class create ::docbuild::object {
     foreach line [split $block \n] {
       set line [string trim $line]
       set fwidx [string first " " $line]
-      set firstword [string range $line 0 [expr {$fwidx-1}]]
+      if {$fwidx < 0} {
+        set firstword [string range $line 0 end]
+        set restline {}
+      } else {
+        set firstword [string range $line 0 [expr {$fwidx-1}]]
+        set restline [string range $line [expr {$fwidx+1}] end]
+      }
       if {[string index $firstword end] eq ":"} {
-        set field [string trim $firstword -]
+        set field [string tolower [string trim $firstword -:]]
         switch $field {
           desc {
             set field description
           }
         }
-        set line [string range $line [expr {$fwidx+1}] end]
+        if {[string length $restline]} {
+          dict append result $field "$restline\n"
+        }
+      } else {
+        dict append result $field "$line\n"
       }
-      dict append result $field "$line\n"
     }
     return $result
   }
@@ -270,49 +348,46 @@ oo::class create ::docbuild::object {
     }
   }
 
-  method section.command {procinfo} {
+  method section.method {keyword method minfo} {
     set result {}
-    putb result "\[section \{Commands\}\]"
-    putb result {[list_begin definitions]}
-    dict for {method minfo} $procinfo {
-      putb result {}
-      set line "\[call proc \[cmd $method\]"
-      if {[dict exists $minfo arglist]} {
-        dict for {argname arginfo} [dict get $minfo arglist] {
-          set positional 1
-          set mandatory  1
-          dict with arginfo {}
-          if {$mandatory==0} {
-            append line " \[opt \""
+    set line "\[call $keyword \[cmd $method\]"
+    if {[dict exists $minfo arglist]} {
+      dict for {argname arginfo} [dict get $minfo arglist] {
+        set positional 1
+        set mandatory  1
+        dict with arginfo {}
+        if {$mandatory==0} {
+          append line " \[opt \""
+        } else {
+          append line " "
+        }
+        if {$positional} {
+          append line "\[arg $argname"
+        } else {
+          append line "\[option $argname"
+          if {[dict exists $arginfo type]} {
+            append line " \[method [dict get $arginfo type]\]"
           } else {
-            append line " "
-          }
-          if {$positional} {
-            append line "\[arg $argname"
-          } else {
-            append line "\[option $argname"
-            if {[dict exists $arginfo type]} {
-              append line " \[cmd [dict get $arginfo type]\]"
-            } else {
-              append line " \[cmd $argname\]"
-            }
-          }
-          append line "\]"
-          if {$mandatory==0} {
-            if {[dict exists $arginfo default]} {
-              append line " \[const \"[dict get $arginfo default]\"\]"
-            }
-            append line "\"\]"
+            append line " \[method $argname\]"
           }
         }
-      }
-      append line \]
-      putb result $line
-      if {[dict exists $minfo description]} {
-        putb result [dict get $minfo description]
+        append line "\]"
+        if {$mandatory==0} {
+          if {[dict exists $arginfo default]} {
+            append line " \[const \"[dict get $arginfo default]\"\]"
+          }
+          append line "\"\]"
+        }
       }
     }
-    putb result {[list_end]}
+    append line \]
+    putb result $line
+    if {[dict exists $minfo description]} {
+      putb result [dict get $minfo description]
+    }
+    if {[dict exists $minfo example]} {
+      putb result "\[example [list [dict get $minfo example]]\]"
+    }
     return $result
   }
 
@@ -328,8 +403,12 @@ oo::class create ::docbuild::object {
       putb result {[para]}
     }
     dict for {f v} $class_info {
-      if {$f in {class_method method description ancestors}} continue
+      if {$f in {class_method method description ancestors example}} continue
       putb result "\[emph \"$f\"\]: $v"
+      putb result {[para]}
+    }
+    if {[dict exists $class_info example]} {
+      putb result "\[example \{[list [dict get $class_info example]]\}\]"
       putb result {[para]}
     }
     if {[dict exists $class_info description]} {
@@ -341,42 +420,7 @@ oo::class create ::docbuild::object {
       #putb result "Methods on the class object itself."
       putb result {[list_begin definitions]}
       dict for {method minfo} [dict get $class_info class_method] {
-        putb result {}
-        set line "\[call method \[cmd $method\]"
-        if {[dict exists $minfo arglist]} {
-          dict for {argname arginfo} [dict get $minfo arglist] {
-            set positional 1
-            set mandatory  1
-            dict with arginfo {}
-            if {$mandatory==0} {
-              append line " \[opt \""
-            } else {
-              append line " "
-            }
-            if {$positional} {
-              append line "\[arg $argname"
-            } else {
-              append line "\[option $argname"
-              if {[dict exists $arginfo type]} {
-                append line " \[method [dict get $arginfo type]\]"
-              } else {
-                append line " \[method $argname\]"
-              }
-            }
-            append line "\]"
-            if {$mandatory==0} {
-              if {[dict exists $arginfo default]} {
-                append line " \[const \"[dict get $arginfo default]\"\]"
-              }
-              append line "\"\]"
-            }
-          }
-        }
-        append line \]
-        putb result $line
-        if {[dict exists $minfo description]} {
-          putb result [dict get $minfo description]
-        }
+        putb result [my section.method classmethod $method $minfo]
       }
       putb result {[list_end]}
       putb result {[para]}
@@ -385,46 +429,22 @@ oo::class create ::docbuild::object {
       putb result "\[class {Methods}\]"
       putb result {[list_begin definitions]}
       dict for {method minfo} [dict get $class_info method] {
-        putb result {}
-        set line "\[call method \[cmd $method\]"
-        if {[dict exists $minfo arglist]} {
-          dict for {argname arginfo} [dict get $minfo arglist] {
-            set positional 1
-            set mandatory  1
-            dict with arginfo {}
-            if {$mandatory==0} {
-              append line " \[opt \""
-            } else {
-              append line " "
-            }
-            if {$positional} {
-              append line "\[arg $argname"
-            } else {
-              append line "\[opt $argname"
-              if {[dict exists $arginfo type]} {
-                append line " \[method [dict get $arginfo type]\]"
-              } else {
-                append line " \[method $argname\]"
-              }
-            }
-            append line "\]"
-            if {$mandatory==0} {
-              if {[dict exists $arginfo default]} {
-                append line " \[const \"[dict get $arginfo default]\"\]"
-              }
-              append line "\"\]"
-            }
-          }
-        }
-        append line \]
-        putb result $line
-        if {[dict exists $minfo description]} {
-          putb result [dict get $minfo description]
-        }
+        putb result [my section.method method $method $minfo]
       }
       putb result {[list_end]}
       putb result {[para]}
     }
+    return $result
+  }
+
+  method section.command {procinfo} {
+    set result {}
+    putb result "\[section \{Commands\}\]"
+    putb result {[list_begin definitions]}
+    dict for {method minfo} $procinfo {
+      putb result [my section.method proc $method $minfo]
+    }
+    putb result {[list_end]}
     return $result
   }
 
@@ -480,25 +500,3 @@ oo::class create ::docbuild::object {
   }
 }
 
-proc ::docbuild::cat filename {
-  set fin [open $filename r]
-  set data [read $fin]
-  close $fin
-  return $data
-}
-
-
-proc ::putb {buffername args} {
-  upvar 1 $buffername buffer
-  switch [llength $args] {
-    1 {
-      append buffer [lindex $args 0] \n
-    }
-    2 {
-      append buffer [string map {*}$args] \n
-    }
-    default {
-      error "usage: putb buffername ?map? string"
-    }
-  }
-}
